c6a596bec337785bad6ff366da009496c269786d
huqc <greyforest.forest@gmail.com>
2017-07-30 21:30:12 +0800

    多次设置时不覆盖config.json

--- php/routes/console.php
+++ php/routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use App\User;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,6 +38,12 @@ Artisan::command('initblog {email} {nick} {pwd}', function ($email, $nick, $pwd)
 
 Artisan::command('initblog_setapipassword', function (){
     $row = DB::table('oauth_clients')->where('password_client', 1)->latest()->first();
-    Storage::put('settings.json', json_encode(['apiclient'=>['id'=>$row->id, 'secret'=>$row->secret], 'title'=>'未设置名称', 'desc'=>'未设置描述']));
+    if($exists = Storage::exists('settings.json')) {   //::disk('local')
+        $config = json_decode(Storage::get('settings.json'));
+        $config->apiclient = ['id'=>$row->id, 'secret'=>$row->secret];
+    } else {
+        $config = ['apiclient'=>['id'=>$row->id, 'secret'=>$row->secret], 'title'=>'未设置名称', 'desc'=>'未设置描述'];
+    }
+    Storage::put('settings.json', json_encode($config));
     DB::table('oauth_clients')->where('password_client', 1)->where('id', '<', $row->id)->delete();
 })->describe('initblog 邮箱 密码');
