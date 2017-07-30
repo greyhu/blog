67a8926e93b41d081dfcba9a411104027660b117
huqc <greyforest.forest@gmail.com>
2017-07-30 17:00:40 +0800

    1

--- php/init.sh
+++ php/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #chown -R :www-data ./
 echo 正在下载依赖程序
-apt install zip unzip nginx php7.0 mysql-server composer php7.0-mbstring php7.0-dom
+apt install zip unzip nginx php7.0 mysql-server composer php7.0-mbstring php7.0-dom php7.0-mysql
 echo 正在设置文件系统权限
 chmod -R 775 ./storage
 chmod -R 775 ./bootstrap/cache
@@ -31,7 +31,7 @@ sed -i "s/DB_PASSWORD=(.*)/DB_PASSWORD=$dbpwd/" .env
 
 echo 正在创建数据库
 MYSQL_CMD="mysql -u${dbuser} -p${dbpwd}"
-create_db_sql="create database IF NOT EXISTS ${dbname}"
+create_db_sql="create database IF NOT EXISTS ${dbname} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
 echo ${create_db_sql}  | ${MYSQL_CMD}
 if [ $? -ne 0 ]; then
 	echo 创建数据库失败，请确认mysql用户密码，或者尝试修改本脚本，为mysql设置正确的host和端口
